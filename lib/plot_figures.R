8a2e1debe79600d31ab1003085957e0a8e9a9722
Zena Lapp <zenalapp@gmail.com>
2020-03-24 13:50:35 -0400

    Add Fig S3 to plot_figures

--- lib/plot_figures.R
+++ lib/plot_figures.R
@@ -401,6 +401,36 @@ save_as_pdf_eps_png("Figure_4A_overlap_functional_impact",
                     default_width,
                     default_height)
 
+
+# Figure S3 Heritability Estimate Differences
+cols = project_key 
+her_ests = read.csv('data/her_ests.csv', header = F, stringsAsFactors = F)
+colors = cols$hex_color
+names(colors) = cols$Project
+proj_name = cols$Dataset
+names(proj_name) = cols$Project
+
+names(her_ests) = c('datname','phen_type','pw_type','her_est')
+
+her_ests$dataset = gsub('_prewas.*','',her_ests$datname)
+her_ests$prewas_run = gsub('.*prewas_output_','',her_ests$datname)
+her_ests$prewas_run[her_ests$prewas_run == 'no_ancestral_reconstruction_no_tree_given'] = 'Major Allele'
+her_ests$prewas_run[her_ests$prewas_run == 'with_ancestral_reconstruction_no_tree_given'] = 'AR + NJ tree'
+her_ests$prewas_run[her_ests$prewas_run == 'with_ancestral_reconstruction_given_tree'] = 'AR + ML tree'
+her_ests$phen_type[her_ests$phen_type == 'bm'] = 'BM'
+her_ests$phen_type[her_ests$phen_type == 'wn'] = 'WN'
+
+he = her_ests[her_ests$pw_type == 'multi',]
+he$diff = (her_ests$her_est[her_ests$pw_type == 'multi'] - her_ests$her_est[her_ests$pw_type == 'no multi'])*100
+
+ggplot(he, aes(x=phen_type,y=diff)) + geom_boxplot() + geom_jitter(aes(col=dataset),size=3) + facet_grid(cols = vars(prewas_run)) + 
+  xlab('Phenotype Evolutionary Model') + ylab('Heritability with multiallelic -\nHeritability without multiallelic (%)') + 
+  scale_color_manual(name = 'Dataset',values = colors, labels = proj_name) + 
+  theme_bw() +
+  theme(text = element_text(size=10))
+
+save_as_pdf_eps_png("Figure_S3_heritability_estimate_differences", 6, 4)
+
 # Figure S3 --------------------------------------------------------------------
 ## S3A Number of Overlapping Genes ----------------------------------------------
 overlap_stats <- left_join(overlap_stats, project_key, by = "Project")
@@ -425,7 +455,7 @@ overlap_stats %>%
   theme(axis.text.x = element_text(angle = 90, hjust = 1, color = "black"),
         axis.text.y = element_text(color = "black"))
 
-save_as_pdf_eps_png("Figure_S4A_overlap_SNP_count",
+save_as_pdf_eps_png("Figure_S5A_overlap_SNP_count",
                     default_width,
                     default_height)
 
@@ -448,12 +478,12 @@ overlap_stats %>%
   theme(legend.position = "none") +
   theme(axis.text.x = element_text(color = "black"),
         axis.text.y = element_text(color = "black"))
-save_as_pdf_eps_png("Figure_S4B_overlap_gene_count",
+save_as_pdf_eps_png("Figure_S5B_overlap_gene_count",
                     default_width,
                     default_height)
 
 
-sink("../data/numbers_for_paper.txt")
+sink("data/numbers_for_paper.txt")
 'Number of datasets:'
 nrow(variant_summary)
 'Number of species:'
