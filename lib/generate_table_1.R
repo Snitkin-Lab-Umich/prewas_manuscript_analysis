2f62c30d680974742e3ec7dcb4ab6530da591d73
Katie Saund <katiephd@umich.edu>
2020-03-25 10:38:14 -0400

    Fix formating, fix relative paths, add description

--- lib/generate_table_1.R
+++ lib/generate_table_1.R
@@ -1,14 +1,25 @@
+# This script aggregates data together for both Table_1 and Table_S1 for the 
+# paper. 
+
 library(tidyverse)
 library(formattable)
 library(htmltools)
 library(webshot)
 
-table1 <- read_tsv("../data/multiallelic_summary.tsv")
+table1 <- read_tsv("data/multiallelic_summary.tsv")
 table1 <- table1 %>%
-  select(Dataset, NumIsolates, NumMultiallelicSite, MeanSNPDist, NumBiallelicOverlappingGene)
-colnames(table1) <- c("Name", "Samples (Count)", "Multiallelic Sites (Count)", "Mean SNP Distance (BP)", "SNPs in overlapping genes (Count)")
+  select(Dataset, 
+         NumIsolates,
+         NumMultiallelicSite,
+         MeanSNPDist, 
+         NumBiallelicOverlappingGene)
+colnames(table1) <- c("Name", 
+                      "Samples (Count)", 
+                      "Multiallelic Sites (Count)", 
+                      "Mean SNP Distance (BP)", 
+                      "SNPs in overlapping genes (Count)")
 
-table1_part2 <- read_tsv("../data/handwritten_Table_1.tsv")
+table1_part2 <- read_tsv("data/handwritten_Table_1.tsv")
 table1_part2 <-
   table1_part2 %>% select("Name",
                           "Bioproject",
@@ -22,6 +33,6 @@ colnames(table1_part2) <- c("Name",
                             "Dataset Description")
 table1 <- left_join(table1_part2, table1, by = c("Name"))
 
-write_tsv(table1, path = "../data/Table_1.tsv")
+write_tsv(table1, path = "data/Table_1.tsv")
 
 
