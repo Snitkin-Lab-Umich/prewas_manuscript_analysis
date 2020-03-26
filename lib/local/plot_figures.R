af46ac40b391ded2199e555a5236c0efcc27cd94
Zena Lapp <zenalapp@gmail.com>
2020-03-26 13:43:47 -0400

    update heritability figure

--- lib/local/plot_figures.R
+++ lib/local/plot_figures.R
@@ -417,38 +417,37 @@ save_as_pdf_eps_png("Figure_4A_overlap_functional_impact",
                     default_height)
 
 
-# Figure S3 Heritability Estimate Differences
+# Figure S4 Heritability Estimate Differences
 cols = project_key 
-her_ests = read.csv('data/her_ests.csv', header = F, stringsAsFactors = F)
+her_ests = read.csv('data/her_ests_shared.csv', header = F, stringsAsFactors = F)
 colors = cols$hex_color
 names(colors) = cols$Project
 proj_name = cols$Dataset
 names(proj_name) = cols$Project
 
-names(her_ests) = c('datname','phen_type','pw_type','her_est')
+names(her_ests) = c('datname','phen_type','ref_type','multi_type','her_est')
 
 her_ests$dataset = gsub('_prewas.*','',her_ests$datname)
-her_ests$prewas_run = gsub('.*prewas_output_','',her_ests$datname)
-her_ests$prewas_run[her_ests$prewas_run == 'no_ancestral_reconstruction_no_tree_given'] = 'Major Allele'
-her_ests$prewas_run[her_ests$prewas_run == 'with_ancestral_reconstruction_no_tree_given'] = 'AR + NJ tree'
-her_ests$prewas_run[her_ests$prewas_run == 'with_ancestral_reconstruction_given_tree'] = 'AR + ML tree'
+#her_ests$prewas_run = gsub('.*prewas_output_with_ancestral_reconstruction_given_tree','',her_ests$datname)
+# her_ests$prewas_run[her_ests$prewas_run == 'no_ancestral_reconstruction_no_tree_given'] = 'Major Allele'
+# her_ests$prewas_run[her_ests$prewas_run == 'with_ancestral_reconstruction_no_tree_given'] = 'AR + NJ tree'
+# her_ests$prewas_run[her_ests$prewas_run == 'with_ancestral_reconstruction_given_tree'] = 'AR + ML tree'
 her_ests$phen_type[her_ests$phen_type == 'bm'] = 'BM'
 her_ests$phen_type[her_ests$phen_type == 'wn'] = 'WN'
+her_ests$ref_type[her_ests$ref_type == 'anc'] = 'Ancestral allele'
+her_ests$ref_type[her_ests$ref_type == 'maj'] = 'Major allele'
+her_ests$ref_type[her_ests$ref_type == 'ref'] = 'Ref. Genome allele'
+her_ests$multi_type[her_ests$multi_type == 'multi'] = 'With multiallelic sites'
+her_ests$multi_type[her_ests$multi_type == 'nomulti'] = 'Without multiallelic sites'
 
-he = her_ests[her_ests$pw_type == 'multi',]
-he$diff = (her_ests$her_est[her_ests$pw_type == 'multi'] - her_ests$her_est[her_ests$pw_type == 'no multi'])*100
+ggplot(her_ests, aes(x=phen_type,y=her_est*100)) + geom_jitter(aes(col=dataset),size=3) + #geom_boxplot() +
+facet_grid(multi_type ~ ref_type) +
+xlab('Phenotype Evolutionary Model') + ylab('Heritability estimate (%)') +
+scale_color_manual(name = 'Dataset',values = colors, labels = proj_name) +
+theme_bw() +
+theme(text = element_text(size=10))
 
-ggplot(he, aes(x = phen_type, y = diff)) + 
-  geom_boxplot() + 
-  geom_jitter(aes(col=dataset),size = 4) + 
-  facet_grid(cols = vars(prewas_run)) + 
-  xlab('Phenotype Evolutionary Model') +
-  ylab('Heritability with multiallelic -\nHeritability without multiallelic (%)') + 
-  scale_color_manual(name = 'Dataset', values = colors, labels = proj_name) + 
-  theme_bw() +
-  theme(text = element_text(size = 10))
-
-save_as_pdf_eps_png("Figure_S3_heritability_estimate_differences", 6, 4)
+save_as_pdf_eps_png("Figure_S4_heritability_estimate_differences", 6, 4)
 
 # Figure S5 --------------------------------------------------------------------
 ## S5A Number of Overlapping Genes ----------------------------------------------
